340aee45ace82b98ccf9eadffffaed017670251f
ShiyuZhang <shiyuzhang2003@gmail.com>
2023-11-11 14:27:54 -0500

    changes to accept recordings

---
+++ recording.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let mediaRecorder;
+    let chunks = [];
+  
+    const recordButton = document.getElementById('recordButton');
+    const audioElement = document.getElementById('audioElement');
+  
+    recordButton.addEventListener('click', () => {
+      if (mediaRecorder && mediaRecorder.state === 'recording') {
+        // Stop recording
+        mediaRecorder.stop();
+        recordButton.textContent = 'Start Recording';
+      } else {
+        // Start recording
+        startRecording();
+        recordButton.textContent = 'Stop Recording';
+      }
+    });
+  
+    function startRecording() {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          mediaRecorder = new MediaRecorder(stream);
+  
+          mediaRecorder.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+              chunks.push(event.data);
+            }
+          };
+  
+          mediaRecorder.onstop = () => {
+            const blob = new Blob(chunks, { type: 'audio/wav' });
+            chunks = [];
+            const audioURL = URL.createObjectURL(blob);
+            audioElement.src = audioURL;
+            return audioURL;
+            // Create a download link
+            // const downloadLink = document.createElement('a');
+            // downloadLink.href = audioURL;
+            // downloadLink.download = 'recorded_audio.wav';
+            // document.body.appendChild(downloadLink);
+            // downloadLink.click();
+            // document.body.removeChild(downloadLink);
+          };
+          mediaRecorder.start();
+        })
+        .catch((error) => {
+          console.error('Error accessing microphone:', error);
+        });
+    }
+  });
