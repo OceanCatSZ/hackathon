57ae7af5b2d433dc40a4a5aff824e3ad1debfce6
music_apprentice66 <jyhuang@umass.edu>
2023-11-11 18:43:04 -0500

    changedRecording to return promise-Jason

--- recording.js
+++ recording.js
@@ -12,30 +12,44 @@ document.addEventListener('DOMContentLoaded', function () {
       recordButton.textContent = 'Start Recording';
     } else {
       // Start recording
-      startRecording();
+      startRecording()
+        .then(blob => {
+          // Use the blob here, e.g., assign it to the audio element
+          const audioURL = URL.createObjectURL(blob);
+          audioElement.src = audioURL;
+
+          // You can also handle the blob in other ways, e.g., sending it to a server
+        })
+        .catch(error => {
+          console.error('Error in recording:', error);
+        });
       recordButton.textContent = 'Stop Recording';
     }
   });
 
   function startRecording() {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then((stream) => {
-        mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.ondataavailable = (event) => {
-          if (event.data.size > 0) {
-            chunks.push(event.data);
-          }
-        };
-        mediaRecorder.onstop = () => {
-          const blob = new Blob(chunks, { type: 'audio/wav' });
-          chunks = [];
-          const audioURL = URL.createObjectURL(blob);
-          audioElement.src = audioURL;
-        };
-        mediaRecorder.start();
-      })
-      .catch((error) => {
-        console.error('Error accessing microphone:', error);
-      });
+    return new Promise((resolve, reject) => {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          mediaRecorder = new MediaRecorder(stream);
+          mediaRecorder.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+              chunks.push(event.data);
+            }
+          };
+          mediaRecorder.onstop = () => {
+            const blob = new Blob(chunks, { type: 'audio/wav' });
+            chunks = [];
+            resolve(blob);
+          };
+          mediaRecorder.onerror = (event) => {
+            reject(event.error);
+          };
+          mediaRecorder.start();
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   }
 });
