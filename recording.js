c2bdc11169b766c04e04f7c281ab84381e69a9a9
ShiyuZhang <shiyuzhang2003@gmail.com>
2023-11-12 00:13:22 -0500

    changes by jay

--- recording.js
+++ recording.js
@@ -59,33 +59,33 @@ function downloadRecording() {
     document.body.removeChild(downloadLink);
 }
 
-function sendMP3(filePath) {
-    const FormData = require('form-data');
-    const fs = require('fs');
-    const axios = require('axios');
+// function sendMP3(filePath) {
+//     const FormData = require('form-data');
+//     const fs = require('fs');
+//     const axios = require('axios');
   
-    const form = new FormData();
+//     const form = new FormData();
   
-    form.append('file', fs.createReadStream(filePath));
+//     form.append('file', fs.createReadStream(filePath));
   
-    axios.post('http://localhost:5000/upload', form, {
-      headers: form.getHeaders(),
-    })
-    .then((response) => {
+//     axios.post('http://localhost:5000/upload', form, {
+//       headers: form.getHeaders(),
+//     })
+//     .then((response) => {
   
-      console.log('From JS: uploaded successfully.', response.data);
-    })
-    .catch((error) => {
-      console.error('Error uploading file', error);
-    });
-  }
-  const filePath = "Recording.mp3"; 
-  console.log("Javascript")
-  sendMP3(filePath);
-  // const FormData 
+//       console.log('From JS: uploaded successfully.', response.data);
+//     })
+//     .catch((error) => {
+//       console.error('Error uploading file', error);
+//     });
+//   }
+//   const filePath = "Recording.mp3"; 
+//   console.log("Javascript")
+//   sendMP3(filePath);
+//   // const FormData 
 
-function handleFile() {
-  console.log("Javascript")
-  const fileInput = document.getElementById('fileInput');
-  sendMP3(fileInput)
-}
+// function handleFile() {
+//   console.log("Javascript")
+//   const fileInput = document.getElementById('fileInput');
+//   sendMP3(fileInput)
+// }
--- sendMP3.js
+++ sendMP3.js
@@ -1,40 +1,20 @@
-// function sendMP3(filePath) {
-//   const FormData = require('form-data');
-//   const fs = require('fs');
-//   const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const axios = require('axios');
 
-//   const form = new FormData();
+const form = new FormData();
+const filePath = "Recording.mp3"; 
 
-//   form.append('file', fs.createReadStream(filePath));
+form.append('file', fs.createReadStream(filePath));
 
-//   axios.post('http://localhost:5000/upload', form, {
-//     headers: form.getHeaders(),
-//   })
-//   .then((response) => {
+axios.post('http://localhost:5000/upload', form, {
+  headers: form.getHeaders(),
+})
+.then((response) => {
+  console.log('File uploaded successfully', response.data);
+})
+.catch((error) => {
+  console.error('Error uploading file', error);
+});
 
-//     console.log('From JS: uploaded successfully.', response.data);
-//   })
-//   .catch((error) => {
-//     console.error('Error uploading file', error);
-//   });
-// }
-// const filePath = "Recording.mp3"; 
-// sendMP3(filePath);
-// // const FormData = require('form-data');
-// const fs = require('fs');
-// const axios = require('axios');
-
-// const form = new FormData();
-// const filePath = "Recording.mp3"; 
-
-// form.append('file', fs.createReadStream(filePath));
-
-// axios.post('http://localhost:5000/upload', form, {
-//   headers: form.getHeaders(),
-// })
-// .then((response) => {
-//   console.log('File uploaded successfully', response.data);
-// })
-// .catch((error) => {
-//   console.error('Error uploading file', error);
-// });
+console.log("js")
