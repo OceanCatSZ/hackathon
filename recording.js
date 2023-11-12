baaa42fe156baa8719c333d9e6c0bf35ec8e207e
ShiyuZhang <shiyuzhang2003@gmail.com>
2023-11-11 21:57:17 -0500

    correction to recording

--- recording.js
+++ recording.js
@@ -1,55 +1,79 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let mediaRecorder;
-  let chunks = [];
+let mediaRecorder;
+let chunks = [];
+let audioURL;
 
-  const recordButton = document.getElementById('recordButton');
-  const audioElement = document.getElementById('audioElement');
+function toggleRecording() {
+    const recordButton = document.getElementById('recordButton');
+    const audioPlayer = document.getElementById('audioPlayer');
+    const downloadButton = document.getElementById('downloadButton');
 
-  recordButton.addEventListener('click', () => {
     if (mediaRecorder && mediaRecorder.state === 'recording') {
-      // Stop recording
-      mediaRecorder.stop();
-      recordButton.textContent = 'Start Recording';
+        // Stop recording
+        mediaRecorder.stop();
+        recordButton.textContent = 'Start Recording';
+        downloadButton.disabled = false;
     } else {
-      // Start recording
-      startRecording()
-        .then(blob => {
-          // Use the blob here, e.g., assign it to the audio element
-          const audioURL = URL.createObjectURL(blob);
-          audioElement.src = audioURL;
+        // Start recording with a fresh chunks array
+        chunks = [];
+        startRecording();
+        recordButton.textContent = 'Stop Recording';
+        downloadButton.disabled = true;
+    }
+}
 
-          // You can also handle the blob in other ways, e.g., sending it to a server
+function startRecording() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream);
+
+            mediaRecorder.ondataavailable = event => {
+                if (event.data.size > 0) {
+                    chunks.push(event.data);
+                }
+            };
+
+            mediaRecorder.onstop = () => {
+                const audioBlob = new Blob(chunks, { type: 'audio/mp3' });
+                const audioUrl = URL.createObjectURL(audioBlob);
+
+                const audioPlayer = document.getElementById('audioPlayer');
+                audioPlayer.src = audioUrl;
+            };
+
+            mediaRecorder.start();
         })
         .catch(error => {
-          console.error('Error in recording:', error);
+            console.error('Error accessing microphone:', error);
         });
-      recordButton.textContent = 'Stop Recording';
-    }
-  });
+}
 
-  function startRecording() {
-    return new Promise((resolve, reject) => {
-      navigator.mediaDevices.getUserMedia({ audio: true })
-        .then((stream) => {
-          mediaRecorder = new MediaRecorder(stream);
-          mediaRecorder.ondataavailable = (event) => {
-            if (event.data.size > 0) {
-              chunks.push(event.data);
-            }
-          };
-          mediaRecorder.onstop = () => {
-            const blob = new Blob(chunks, { type: 'audio/wav' });
-            chunks = [];
-            resolve(blob);
-          };
-          mediaRecorder.onerror = (event) => {
-            reject(event.error);
-          };
-          mediaRecorder.start();
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+function downloadRecording() {
+    const audioBlob = new Blob(chunks, { type: 'audio/mp3' });
+    const downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(audioBlob);
+    downloadLink.download = 'recorded_audio.mp3';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+
+function handleFile() {
+  const fileInput = document.getElementById('fileInput');
+  if (fileInput.files.length > 0) {
+      const selectedFile = fileInput.files[0];
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+          // The content of the file is stored in e.target.result
+          const fileContent = e.target.result;
+
+          // You can now use the file content as needed
+          console.log('File Content:', fileContent);
+      };
+
+      // Read the file as text
+      reader.readAsText(selectedFile);
+  } else {
+      console.error('No file selected.');
   }
-});
+}
