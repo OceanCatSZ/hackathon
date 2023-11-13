ed5754cefb60ad81b3f3a37420e5ea48a551a1d7
music_apprentice66 <jyhuang@umass.edu>
2023-11-13 16:27:40 -0500

    Jason removed unecessary files

--- app.js
+++ app.js
@@ -13,21 +13,6 @@ myAudio.onplaying = function() {
 myAudio.onpause = function() {
   isPlaying = false;
 };
-// ends here
-//unshul
-
-// Catherine Ryu, Mandarin Tone Perception & Production Team, and Michigan State University Libraries. "mā by FV1." Tone Perfect: Multimodal Database for Mandarin Chinese. Accessed 1 January 2022. https://tone.lib.msu.edu/tone/1187
-
-// function callPython() {
-//   fetch("http://localhost:5000/drawGraph")
-//   .then(response => response.text())
-//   .then(data => {
-//       const responseDiv = document.getElementById('response');
-//       responseDiv.innerHTML = data;
-//       console.log()
-//   })
-//   .catch(error => console.error('Error:', error));
-// }
 
 document.addEventListener("DOMContentLoaded", function() {
   const numbers = [1, 2, 3, 4, 5, 6];
--- receiveImage.js
+++
@@ -1,9 +0,0 @@
-fetch('http://localhost:5000/get_image')
-    .then(response => response.json())
-    .then(data => {
-        const imageBase64 = data.image;
-        document.getElementById('displayedImage').src = `data:image/jpeg;base64,${imageBase64}`;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
